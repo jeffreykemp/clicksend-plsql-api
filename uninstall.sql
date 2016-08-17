c65f5d9d24df8116e75e55e5c7debeb893629767
Jeffrey Kemp <jeffrey.kemp@jk64.com>
2016-08-17 14:23:39 +0800

    clicksend v0.1

---
+++ uninstall.sql
@@ -0,0 +1,38 @@
+prompt uninstall.sql
+-- v0.1
+
+prompt drop job (process queue)
+begin dbms_scheduler.stop_job ('clicksend_process_queue'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
+/
+begin dbms_scheduler.drop_job('clicksend_process_queue'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
+/
+
+prompt drop job (purge logs)
+begin dbms_scheduler.stop_job ('clicksend_purge_logs'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
+/
+begin dbms_scheduler.drop_job('clicksend_purge_logs'); exception when others then if sqlcode not in (-27366,-27475) then raise; end if; end;
+/
+
+prompt drop queue
+begin dbms_aqadm.stop_queue (user||'.clicksend_queue'); exception when others then if sqlcode!=-24010 then raise; end if; end;
+/
+begin dbms_aqadm.drop_queue (user||'.clicksend_queue'); exception when others then if sqlcode!=-24010 then raise; end if; end;
+/
+begin dbms_aqadm.drop_queue_table (user||'.clicksend_queue_tab'); exception when others then if sqlcode not in (-24010,-24002) then raise; end if; end;
+/
+
+prompt drop tables
+begin execute immediate 'drop table clicksend_msg_log'; exception when others then if sqlcode!=-942 then raise; end if; end;
+/
+begin execute immediate 'drop table clicksend_settings'; exception when others then if sqlcode!=-942 then raise; end if; end;
+/
+
+prompt drop types
+begin execute immediate 'drop type t_clicksend_msg'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+
+prompt drop package
+begin execute immediate 'drop package clicksend_pkg'; exception when others then if sqlcode!=-4043 then raise; end if; end;
+/
+
+prompt finished.
